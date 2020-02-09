bf04976e327dcbd85692e22fee9e094bbe43875f
Aladdin Selmi <selmi.aladdin@gmail.com>
2020-02-09 19:00:09 +0100

    Modified Structure

---
+++ src/pages/Home.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .home
+    img(alt="Vue logo" src="../assets/logo.png")
+    h3 LANDING PAGE
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
---
+++ src/pages/Search.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div.hello SEARCH
+</template>
+
+<script>
+export default {
+
+}
+</script>
